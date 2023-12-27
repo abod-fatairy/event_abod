2bfe5d99d6a4d00bd762ea540314bf2f8a3bd33f
abodfatairy <abodfatairy@gmail.com>
2023-12-27 12:28:40 +0200

    first commit

--- lib/mongoDb/actions/user.actions.ts
+++ lib/mongoDb/actions/user.actions.ts
@@ -2,10 +2,10 @@
 
 import { revalidatePath } from 'next/cache'
 
-import { connectToDatabase } from '@/lib/database'
-import User from '@/lib/database/models/user.model'
-import Order from '@/lib/database/models/order.model'
-import Event from '@/lib/database/models/event.model'
+import { connectToDatabase } from '../database'
+import User from '../models/user.model'
+import Order from '../models/order.model'
+import Event from '../models/event.model'
 import { handleError } from '@/lib/utils'
 
 import { CreateUserParams, UpdateUserParams } from '@/types'
